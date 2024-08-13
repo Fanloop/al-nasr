114524a694e22148903f9285b211903dbeef1a97
Fanloop <fahriprayoga3010@gmail.com>
2024-08-13 15:26:14 +0700

    add comment to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import About from "./components/About";
+// import About from "./components/About";
 import Facilities from "./components/Facilities";
-import Footer from "./components/Footer";
-import Gallery from "./components/Gallery";
+// import Footer from "./components/Footer";
+// import Gallery from "./components/Gallery";
 import Navbar from "./components/Navbar";
-import Package from "./components/Package";
-import Sponsor from "./components/Sponsor";
+// import Package from "./components/Package";
+// import Sponsor from "./components/Sponsor";
 import Hero from "./components/Hero";
 
 function App() {
